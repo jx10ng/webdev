adee976d0515a8a15739835b83b10125fae9a79c
Julia Xiong <julxiong@gmail.com>
2017-03-05 23:32:18 -0800

    Modifications to HW4. Array of Objects dynamically inserted. Ability to delete text with delete button.

--- HW4/docRoot/purchase.js
+++ HW4/docRoot/purchase.js
@@ -1,164 +1,178 @@
 //order details
-var order = [];
-	order[0] = {description: "Hat", price: 200, partNumber: "1000"}
-	order[1] = {description: "Shoe", price: 20, partNumber: "1001"}
-	order[2] = {description: "Pants", price:100, partNumber: "1002"}
-	order[3] = {description: "Shirt", price:10, partNumber: "1003"}
-alert("order: " + JSON.stringify(order));
+var order = {
+	"itemlist": {
+		"item1":{
+			"description": "Hat", 
+			"price": 200,
+			"quantity": 1
+		},
+		"item2": {
+			"description": "Shoe", 
+			"price": 20,
+			"quantity":1
+		},
+		"item3":{
+			"description": "Pants", 
+			"price": 100,
+			"quantity":1
+		},
+		"item4": {
+			"description": "Shirt", 
+			"price": 10,
+			"quantity":1
+		},
+		"item5": {
+			"description": "Dress", 
+			"price": 100,
+			"quantity":1
+		}
+	},
+	"customer": {
+		"name": "Grey Wilson",
+		"address": "1234 Strawberry Creek, Berkeley, CA 94704"
+	}
+}
 
-
-//customer details
-var customer = {name: "Grey Wilson", address: "1234 Strawberry Creek, Berkeley, CA 94704"}
+//alert("order: " + JSON.stringify(order));
 
 
 //onload, insert all customer and item details into HTML with this function
 window.onload = function (){
 	//insert customer name
 	var customer_name = document.getElementById("custname");
-	customer_name.textContent = customer.name;
+	customer_name.textContent = order.customer.name;
 	
 	//insert customer address
 	var customer_add = document.getElementById("custadd");
-	customer_add.textContent = customer.address;
+	customer_add.textContent = order.customer.address;
 
-	//insert item details with description and price
-	for (i = 0; i < order.length; i++){
-		num = 1 + i;
-		document.getElementById("item" + num).textContent = order[i].description;
-		document.getElementById("amt" + num).textContent = order[i].price
-	}
-
-	//finding items cost
-	var sum = 0;
-	for (i = 0; i<order.length; i++){
-		sum += order[i].price;
-	}
-	//inserting item cost
-	var item_amt = document.getElementById("itemsamt");
-	item_amt.textContent = sum;
-	//inserting tax cost
-	var tax_amt = document.getElementById("taxamt");
-	tax_amt.textContent = sum*0.1;
-	//inserting shipping cost
-	var ship_amt = document.getElementById("shipamt");
-	ship_amt.textContent = 5;
-	//inserting total purchase cost
-	var total_amt = document.getElementById("totalamt");
-	total_amt_val = Number(item_amt.textContent) + Number(tax_amt.textContent)+ Number(ship_amt.textContent);
-	total_amt.textContent = total_amt_val;
-
-	//assigning event listeners to delete buttons
-	var item1dlt = document.getElementById("btndlt1"); //
-	item1dlt.addEventListener("click", removerow1); //on a click, run fDelete function on item 1
-	item1dlt.addEventListener("click",recal);
-	//item1dlt.addEventListener("click", myIndex);
-
-	var item2dlt = document.getElementById("btndlt2"); //
-	item2dlt.addEventListener("click", removerow2); //on a click, run fDelete function on item 2
-	item2dlt.addEventListener("click",recal);
-	//item2dlt.addEventListener("click", myIndex);
-
-	var item3dlt = document.getElementById("btndlt3"); //
-	item3dlt.addEventListener("click", removerow3); //on a click, run fDelete function on item 3
-	item3dlt.addEventListener("click",recal);
-
-	var item4dlt = document.getElementById("btndlt4"); //
-	item4dlt.addEventListener("click", removerow4); //on a click, run fDelete function on item 4
-	item4dlt.addEventListener("click",recal);
-}
-
-
-
-//Delete function
-function fDelete(){
-	alert("delete me");
-}
-
-/*
-function myIndex(){
-	var x = document.getElementsByTagName("tr");
-	a = 0
-	alert("my this: " + JSON.stringify(x));
-	alert("Row index is: " + x[a].rowIndex);
-}
-
-*/
-
-
-//Delete item1 from item table
-function removerow1() {
-	var myrow = document.getElementById("row1");
-	while (myrow.firstChild) myrow.removeChild(myrow.firstChild);
-	//alert("myrow1 index: " + myrow.rowindex)
-	//Remove item 1 from object
-	/*alert("order index: " + order.indexOf("item1dlt"));*/
-	a = 0;//order.index;
-	b = 1;//order.index + 1;
-	order.splice(a,b); 
-	/*alert("order row1: " + JSON.stringify(order));
-	alert("order length: " + order.length);*/
+	//identify div that will store purchase details
+	list = document.getElementById("mydiv");
 	
+	//create Purchase Details with for loop
+	for (i = 0; i < Object.keys(order.itemlist).length; i++){
+		var num = i + 1;
+		
+		//create a new div node for items details
+		newitemdiv = document.createElement("div");
+		newitemdiv.id = "itemdiv" + num;
+		newitemdiv.className = "divdisplay";
+		
+		//create a string for the descritpion, price, and quantity
+		var myitem = "Description: " + order.itemlist["item"+num].description + " Price: $" + order.itemlist["item"+num].price + " Quantity: " + order.itemlist["item"+num].quantity;
+		
+		//create a textnode for the description, price, and quantity
+		newitemtext = document.createTextNode(myitem);
+		
+		//append textnode to div node
+		newitemdiv.appendChild(newitemtext);
+		
+		//append div node (with text) to Purchase Details div node
+		list.appendChild(newitemdiv);		
+
+
+		//create a input node that is a button
+		newinput = document.createElement("input");
+		newinput.type = "button";
+		newinput.value = "Delete Quantity";
+		newinput.id = "btndlt" + num;
+		newinput.index = num;
+		//newinput.addEventListener('click', function(e) { fDelete1(e.target.index); }); //target references the element, newinput
+		newinput.addEventListener('click', fDelete);
+		
+		//create a new div nod for input
+		newinputdiv = document.createElement("div");
+		newinputdiv.id = "btndiv" + num;
+		newinputdiv.className = "divdisplay";
+
+		//append button to paragraph 
+		newinputdiv.appendChild(newinput);
+		
+		//append div node (with input button) to Purchase Details div node
+		list.appendChild(newinputdiv);
+	}
+
+	recal(); //calculate the Purchase Summary
+
 }
 
-//Delete item2 from item table
-function removerow2() {
-	var myrow = document.getElementById("row2");
-	while (myrow.firstChild) myrow.removeChild(myrow.firstChild);
-	//Remove item 1 from object
-	/*alert("order index: " + order.indexOf("item2dlt"));*/
-	a = 1;//order.index;
-	b = 1;//order.index + 1;
-	order.splice(a,b); 
-	//alert("order row1: " + JSON.stringify(order));
-	//alert("order length: " + order.length);
-}
 
-//Delete item3 from item table
-function removerow3() {
-	var myrow = document.getElementById("row3");
-	while (myrow.firstChild) myrow.removeChild(myrow.firstChild);
-	//Remove item 1 from object
-	/*alert("order index: " + order.indexOf("item2dlt"));*/
-	a = 2;//order.index;
-	b = 2;//order.index + 1;
-	order.splice(a,b); 
-	//alert("order row1: " + JSON.stringify(order));
-	//alert("order length: " + order.length);
-}
+//delete items from Purchase Details
+function fDelete(e){ //e is for event
+	num = e.target.index; // index of the element that triggered the event
+	//if quantity > 1
+	//remove one quantity from item in itemlist object
+	//alert("num: " + num); /*for testing purposes*/
+	if (order.itemlist["item"+num].quantity > 0) {
+		//delete 1 quantity
+		order.itemlist["item"+num].quantity -= 1
+		//alert("quantity: " + order.itemlist["item"+num].quantity);	
 
-//Delete item3 from item table
-function removerow4() {
-	var myrow = document.getElementById("row4");
-	while (myrow.firstChild) myrow.removeChild(myrow.firstChild);
-	//Remove item 1 from object
-	/*alert("order index: " + order.indexOf("item2dlt"));*/
-	a = 3;//order.index;
-	b = 3;//order.index + 1;
-	order.splice(a,b); 
-	//alert("order row1: " + JSON.stringify(order));
-	//alert("order length: " + order.length);
+		//Change the display of quanity on screen
+		newtext = "Description: " + order.itemlist["item"+num].description + " Price: $" + order.itemlist["item"+num].price + " Quantity: " + order.itemlist["item"+num].quantity;
+		//create textnode
+		newtextnode = document.createTextNode(newtext); 
+		
+		//indicate div for text details
+		var changediv = document.getElementById("itemdiv" + num); 
+
+		//change text background color for testing purposes
+		//changediv.style.backgroundColor = "orange";
+
+		//replace with new item details
+		changediv.replaceChild(newtextnode, changediv.childNodes[0]);
+
+	}
+	//if quanity < 1
+	if (order.itemlist["item"+num].quantity < 1) {
+		//indicate div for text details
+		var changediv = document.getElementById("itemdiv" + num); 
+
+		//indicate div for button
+		var btndiv = document.getElementById("btndiv" + num); 
+
+		//disable display of text details
+		changediv.style.display = "none";
+
+		//disable display of button
+		btndiv.style.display = "none";
+	}
+	recal();
 }
 
 
 //recalculate Purchase Summary
 function recal(){
 	var sum = 0;
-		for (i = 0; i<order.length; i++){
-			sum += order[i].price;
+		for (i = 0; i<Object.keys(order.itemlist).length; i++){
+			num = i+1; 
+			sum += (order.itemlist["item"+num].price)*(order.itemlist["item"+num].quantity);
 		}
+
 	//inserting item cost
 	var item_amt = document.getElementById("itemsamt");
 	item_amt.textContent = sum;
+	
 	//inserting tax cost
 	var tax_amt = document.getElementById("taxamt");
 	tax_amt.textContent = sum*0.1;
+	
 	//inserting shipping cost
 	var ship_amt = document.getElementById("shipamt");
-	ship_amt.textContent = 5;
+	if (sum == 0 ) {
+		ship_amt.textContent = 0; //no cost for shipping if there are no items in list
+	}
+	else {
+		ship_amt.textContent = 5; 
+	}
+	
 	//inserting total purchase cost
 	var total_amt = document.getElementById("totalamt");
 	total_amt_val = Number(item_amt.textContent) + Number(tax_amt.textContent)+ Number(ship_amt.textContent);
 	total_amt.textContent = total_amt_val;
+
+
 }
+
 
 
